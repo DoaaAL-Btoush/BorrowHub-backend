dbf06124fd014ef16c70fc3d9d2d0262b9875e50
Doaa <doaasuliman371@gmail.com>
2026-06-16 11:00:36 +0300

    Prepare

--- db.js
+++ db.js
@@ -4,11 +4,21 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = new pg.Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  connectionString: process.env.DATABASE_URL
+    ? process.env.DATABASE_URL
+    : undefined,
+
+  host: process.env.DATABASE_URL ? undefined : process.env.DB_HOST,
+  port: process.env.DATABASE_URL ? undefined : process.env.DB_PORT,
+  database: process.env.DATABASE_URL ? undefined : process.env.DB_NAME,
+  user: process.env.DATABASE_URL ? undefined : process.env.DB_USER,
+  password: process.env.DATABASE_URL ? undefined : process.env.DB_PASSWORD,
+
+  ssl: process.env.DATABASE_URL
+    ? {
+        rejectUnauthorized: false,
+      }
+    : false,
 });
 
 export default pool;
